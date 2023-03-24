2f09e1fbf5a01e37657c76641d57bf1168206a5e
Howlin Bash <howlinbash@posteo.de>
2023-03-24 15:00:19 +0200

    Comment out ssh keychain agent cruft

--- .bashrc.d/scripts.sh
+++ .bashrc.d/scripts.sh
@@ -1,11 +1,13 @@
 # Maintain an up to date list of every package installed on laptop
 pacman -Qqe > ~/.notes/pkglist.txt
 
-# Start ssh-agent on login
-if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-    ssh-agent > ~/.ssh-agent-thing
-    sed -i '3d' ~/.ssh-agent-thing
-fi
-if [[ "$SSH_AGENT_PID" == "" ]]; then
-    eval "$(<~/.ssh-agent-thing)"
-fi
+# # Start ssh-agent on login
+# if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+#     ssh-agent > ~/.ssh-agent-thing
+#     sed -i '3d' ~/.ssh-agent-thing
+# fi
+# if [[ "$SSH_AGENT_PID" == "" ]]; then
+#     eval "$(<~/.ssh-agent-thing)"
+# fi
+
+eval `keychain --agents ssh --eval id_rsa_git`

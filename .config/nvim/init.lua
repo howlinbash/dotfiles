9c19907ced821128ef6b1545a17e10841c2fa2c9
Howlin Bash <howlinbash@posteo.de>
2023-03-30 16:28:00 +0300

    Move packer to init

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,3 @@
--- require('hb')
-
--- local todo = vim.fn.expand("~/.config/nvim/todo/todo.vim")
--- local todo_syntax = vim.fn.expand("~/.config/nvim/syntax/todo.vim")
-
--- vim.cmd("source " .. todo)
--- -- vim.cmd("syntax on")
--- -- vim.cmd("syntax enable")
--- vim.cmd("source " .. todo_syntax)
---
 -- Install packer
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 local is_bootstrap = false
@@ -18,57 +8,57 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
 end
 
 require('packer').startup(function(use)
-	-- Package manager
-	use 'wbthomason/packer.nvim'
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		requires = {
-			-- LSP Support
-			'neovim/nvim-lspconfig',             -- Required
-			'williamboman/mason.nvim',           -- Optional
-			'williamboman/mason-lspconfig.nvim', -- Optional
+  -- Package manager
+  use 'wbthomason/packer.nvim'
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      'neovim/nvim-lspconfig', -- Required
+      'williamboman/mason.nvim', -- Optional
+      'williamboman/mason-lspconfig.nvim', -- Optional
 
-			-- Autocompletion
-			'hrsh7th/nvim-cmp',         -- Required
-			'hrsh7th/cmp-nvim-lsp',     -- Required
-			'hrsh7th/cmp-path',         -- Optional
-			'hrsh7th/cmp-buffer',       -- Optional
-			'hrsh7th/cmp-nvim-lua',     -- Optional
-			'saadparwaiz1/cmp_luasnip', -- Optional
+      -- Autocompletion
+      'hrsh7th/nvim-cmp', -- Required
+      'hrsh7th/cmp-nvim-lsp', -- Required
+      'hrsh7th/cmp-path', -- Optional
+      'hrsh7th/cmp-buffer', -- Optional
+      'hrsh7th/cmp-nvim-lua', -- Optional
+      'saadparwaiz1/cmp_luasnip', -- Optional
 
-			-- Snippets
-			'L3MON4D3/LuaSnip',             -- Required
-			'rafamadriz/friendly-snippets', -- Optional
+      -- Snippets
+      'L3MON4D3/LuaSnip', -- Required
+      'rafamadriz/friendly-snippets', -- Optional
 
       -- Useful status updates for LSP
       'j-hui/fidget.nvim',
 
       -- Additional lua configuration, makes nvim stuff amazing
       'folke/neodev.nvim',
-		}
-	}
+    }
+  }
 
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		{ run = function()
-			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-			ts_update()
-      end
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    { run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end
     },
-	}
+  }
 
   use { -- Additional text objects via treesitter
     'nvim-treesitter/nvim-treesitter-textobjects',
     after = 'nvim-treesitter',
   }
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
-		-- or                          , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    -- or                          , branch = '0.1.x',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
 
-	use 'francoiscabrol/ranger.vim' -- ranger fm
+  use 'francoiscabrol/ranger.vim' -- ranger fm
   use 'junegunn/fzf.vim' -- fzf
   use 'plasticboy/vim-markdown' -- markdown
   use 'prettier/vim-prettier' -- prettier
@@ -97,7 +87,7 @@ require('packer').startup(function(use)
   -- use 'NLKNguyen/papercolor-theme'
   -- use 'rakr/vim-one'
   -- use { "catppuccin/nvim", as = "catppuccin" }
--- use { 'EdenEast/nightfox.nvim', as = 'nightfox' }
+  -- use { 'EdenEast/nightfox.nvim', as = 'nightfox' }
   if is_bootstrap then
     require('packer').sync()
   end
@@ -113,5 +103,15 @@ if is_bootstrap then
   print '       then restart nvim'
   print '=================================='
   return
-end
+else
+  require('hb')
 
+  local todo = vim.fn.expand("~/.config/nvim/todo/todo.vim")
+  local todo_syntax = vim.fn.expand("~/.config/nvim/syntax/todo.vim")
+
+  vim.cmd("source " .. todo)
+  -- vim.cmd("syntax on")
+  -- vim.cmd("syntax enable")
+  vim.cmd("source " .. todo_syntax)
+
+end
